341f93f4b9f31f21946086baeba0a2c25c0b968a
addymahadhir <4ddyuta@gmail.com>
2026-02-14 02:00:24 +0800

    Initial commit: Laravel project setup

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Create the specific admin user
+        User::updateOrCreate(
+            ['email' => 'admin@ftos.com'],
+            [
+                'name' => 'System Admin',
+                'password' => Hash::make('truckadmin111_'),
+                'email_verified_at' => now(),
+            ]
+        );
+    }
+}
